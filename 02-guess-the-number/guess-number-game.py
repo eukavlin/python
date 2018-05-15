f495e3e4dd73e54cca0459c035951619c2911211
Élise <brodin.elise@gmail.com>
2018-05-15 11:55:51 +0200

    Exercise 2 - 'Guess the number' game : format with name and guess number parameters

--- 02-guess-the-number/guess-number-game.py
+++ 02-guess-the-number/guess-number-game.py
@@ -7,6 +7,9 @@ print('----------------------------\n      Guess the number\n-------------------
 number = random.randint(0, 100)
 guess = -1
 
+# Ask for player's name
+name = input('Hi player, what is your name ? ')
+
 while guess != number:
 
     # Now, we ask the user to input a random number between 0 and 100 and convert it into an integer
@@ -15,10 +18,10 @@ while guess != number:
 
     # Checking if number and guess are equals.
     if guess > number:
-        print('Your guess of ' + guess_text + ' is too high, try again')
+        print('Sorry {}, your guess of {} is too high, try again'.format(name, guess))
         guess_text
     elif guess < number:
-        print('Your guess of ' + guess_text + ' is too low, try again')
+        print('Sorry {}, your guess of {} is too low, try again'.format(name, guess))
         guess_text
     else:
-        print('Congratulations, the correct number is ' + guess_text)
+        print('Congratulations {}, the correct number was {}. Good job!'.format(name, guess))
